74bb4b884ccaa537febdf7eb917277e62156765d
h-93 <hanif234@hotmail.com>
2023-10-19 21:28:52 +0100

    Initial Commit

---
+++ likeOrDislike/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  likeOrDislike
+//
+//  Created by hanif hussain on 19/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
