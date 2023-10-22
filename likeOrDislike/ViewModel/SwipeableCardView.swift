9c6b915673206e984600a5d87f5856615575772a
h-93 <hanif234@hotmail.com>
2023-10-22 19:42:48 +0100

    Added swipe capabilities by adding 3rd party library

---
+++ likeOrDislike/ViewModel/SwipeableCardView.swift
@@ -0,0 +1,8 @@
+//
+//  SwipeableCardViewDataSource.swift
+//  likeOrDislike
+//
+//  Created by hanif hussain on 20/10/2023.
+//
+
+import Foundation
--- likeOrDislike/Views/ViewController.swift
+++ likeOrDislike/Views/ViewController.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        tabBar.isTranslucent = false
+        tabBar.barTintColor = .orange
+        
+        
     }
 
 
